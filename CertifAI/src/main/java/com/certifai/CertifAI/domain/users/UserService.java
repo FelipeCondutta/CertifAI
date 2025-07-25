9a5128d255358a0b07aea4566fa2a3b09e54708c
unknown <davialealves@gmail.com>
2025-07-24 23:49:48 -0300

    UserService fix

--- CertifAI/src/main/java/com/certifai/CertifAI/domain/users/UserService.java
+++ CertifAI/src/main/java/com/certifai/CertifAI/domain/users/UserService.java
@@ -2,6 +2,9 @@ package com.certifai.CertifAI.domain.users;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class UserService {
@@ -17,4 +20,18 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAll(){
+        return (List<User>) userRepository.findAll();
+    }
+
+    public void deleteUser(Long id){
+        if(!userRepository.existsById(id)){
+            throw new RuntimeException("User not found");
+        }
+        userRepository.deleteById(id);
+    }
 }
